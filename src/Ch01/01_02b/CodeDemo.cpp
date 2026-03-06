96fd9ee26dd4bc9f4cf4cba8afcc266edb55cf9c
Atsuhiko Cook <tech@cookra.co.uk>
2026-03-06 20:47:32 +0000

    Hello World Done!

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,13 @@
 // Complete Guide to C++ Programming Foundations
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+
+#include <iostream>
+
+int main(){
+    std::cout << "Yo World 1" << std::endl;
+    std::cout << "Yo World 2" << std::endl;
+    std::cout << "Yo World 3" << std::endl;
+    return 0;
+
+}
